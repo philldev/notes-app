a12f8ef41ca96de7882c465a144a5eab470b579a
Deddy Wolley <deddywolley@gmail.com>
2021-09-01 14:22:32 +0800

    added unsplash

--- lib/NoteEditor/Components/NoteEditorCover/NoteEditorCover.tsx
+++ lib/NoteEditor/Components/NoteEditorCover/NoteEditorCover.tsx
@@ -4,6 +4,7 @@ import { Popover, Tab } from '@headlessui/react'
 import { ChatAltIcon } from '@heroicons/react/outline'
 import { FromImageLink, ImageUpload } from '.'
 import { useNoteEditor } from '../../Context/NoteEditorContext'
+import UnsplashPhotos from './Unsplash'
 
 const NoteEditorCover = () => {
 	const {
@@ -52,6 +53,30 @@ const NoteEditorCover = () => {
 														</>
 													)}
 												</Tab>
+												<Tab className='relative px-2 h-9'>
+													{({ selected }) => (
+														<>
+															<span className='flex items-center'>
+																<svg
+																	width={16}
+																	height={16}
+																	viewBox='0 0 16 16'
+																	fill='none'
+																	className='mr-2'
+																>
+																	<path
+																		d='M5 4.5V0H11V4.5H5ZM11 7H16V16H0V7H5V11.5H11V7Z'
+																		fill='currentColor'
+																	/>
+																</svg>
+																Unsplash
+															</span>
+															{selected && (
+																<div className='absolute bottom-0 left-0 right-0 h-1 bg-accent-primary' />
+															)}
+														</>
+													)}
+												</Tab>
 											</Tab.List>
 											<Tab.Panels>
 												<Tab.Panel className='p-2'>
@@ -69,6 +94,9 @@ const NoteEditorCover = () => {
 															props.close()
 														}}
 													/>
+												</Tab.Panel>
+												<Tab.Panel>
+													<UnsplashPhotos />
 												</Tab.Panel>
 											</Tab.Panels>
 										</Tab.Group>
---
+++ lib/NoteEditor/Components/NoteEditorCover/Unsplash.tsx
@@ -0,0 +1,120 @@
+import { FC, useEffect, useState } from 'react'
+import { createApi } from 'unsplash-js'
+import Image from 'next/image'
+
+const unsplashApi = createApi({
+	// TODO: update accessKey to env
+	accessKey: '3ctZ3wkOncfF-F00YqcAFuKkJfnGewIXrAupIMHzu-M',
+})
+
+type UnsplashPhoto = {
+	alt_description: string
+	blur_hash: string
+	categories: string[]
+	color: string
+	created_at: string
+	current_user_collections: object[]
+	description: string
+	downloads: number
+	exif: object
+	height: number
+	id: string
+	liked_by_user: boolean
+	likes: number
+	links: {
+		self: string
+		html: string
+		download: string
+		download_location: string
+	}
+	location: {
+		title: string
+		name: string
+		city: string
+		country: 'United States'
+		position: object
+	}
+	promoted_at: string
+	sponsorship: null | string
+	topic_submissions: object
+	updated_at: string
+	urls: {
+		raw: string
+		full: string
+		regular: string
+		small: string
+		thumb: string
+	}
+	user: {
+		id: string
+		updated_at: string
+		username: string
+		name: string
+		first_name: string
+	}
+	views: 197475
+	width: 6336
+}
+
+const useUnsplash = () => {
+	const [state, setState] = useState<{
+		isLoading: boolean
+		isError: boolean
+		photos: UnsplashPhoto[]
+	}>({
+		isLoading: false,
+		photos: [],
+		isError: false,
+	})
+
+	const toggleError = () =>
+		setState((prevState) => ({ ...prevState, isError: !prevState.isError }))
+	const toggleLoading = () =>
+		setState((prevState) => ({ ...prevState, isError: !prevState.isLoading }))
+	const updatePhotos = (photos: UnsplashPhoto[]) =>
+		setState((prevState) => ({ ...prevState, photos }))
+
+	const { isLoading, isError, photos } = state
+
+	useEffect(() => {
+		unsplashApi.photos.getRandom({ count: 5 }).then((result) => {
+			toggleLoading()
+			if (result.type === 'success') {
+				let photos = result.response as any
+				updatePhotos(photos)
+				toggleLoading()
+			} else if (result.type === 'error') {
+				toggleError()
+			}
+		})
+	}, [])
+
+	return { isLoading, photos, isError }
+}
+
+interface UnsplashPhotosProps {}
+
+const UnsplashPhotos: FC<UnsplashPhotosProps> = () => {
+	const { isError, isLoading, photos } = useUnsplash()
+	if (isLoading) {
+		return <div>Loading...</div>
+	}
+
+	return (
+		<div className='grid grid-cols-2 gap-1 p-2'>
+			{photos.map((p, i) => (
+				<div className='relative h-20 cursor-pointer' key={i}>
+					<Image
+						className=''
+						layout='fill'
+						objectFit='cover'
+						src={p.urls.thumb}
+						alt='photo header'
+					/>
+				</div>
+			))}
+		</div>
+	)
+}
+
+export default UnsplashPhotos
